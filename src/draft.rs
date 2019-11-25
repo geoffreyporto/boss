cd6149ae3b5293ced6bc86aa0c1c8b8a3836a1ef
Eli Ben-Porat <eli.bp@jottabyte.io>
2019-11-25 00:32:44 -0500

    Updated player caching

---
+++ src/draft.rs
@@ -0,0 +1,19 @@
+//! API for gathering information from http://statsapi.mlb.com/api/v1/draft/
+//! Years 2017 onward contain signing bonus data, before that only draft position
+//! 
+//! 2016 data with bonuses: https://www.mlb.com/news/2016-draft-signing-and-bonus-tracker-c183906680
+//! 2015 data with bonuses: https://www.mlb.com/news/c-130246508
+//! 
+//! Baseball America has data for years before that, but unclear if data can be used
+//! 
+//! 
+//! 
+
+use serde::Deserialize;
+
+/// This represents the struture from the mlb API
+struct Draft {
+
+
+}
+
